f4e703522c2f95229beaf9ff41dbaf1242c4bfbe
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-12-10 20:12:47 +0000

    Put the Engine struct and impl in it's own file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+mod engine;
+pub use engine::Engine;
+
 use std::error::Error;
 
-use sdl2::{event::Event, render::Canvas, video::Window};
+use sdl2::{render::Canvas, video::Window};
 
 pub use sdl2::{
     pixels::Color,
@@ -15,86 +18,4 @@ pub trait Application {
         canvas: &mut WindowCanvas,
         elapsed_time: f64,
     ) -> Result<(), Box<dyn Error>>;
-}
-
-pub struct Engine<'a> {
-    app: Box<dyn Application>,
-    title: &'a str,
-    ctx: sdl2::Sdl,
-    video: sdl2::VideoSubsystem,
-    timer: sdl2::TimerSubsystem,
-    canvas: WindowCanvas,
-}
-
-impl<'a> Engine<'a> {
-    pub fn new(
-        app: Box<dyn Application>,
-        title: &'a str,
-        width: u32,
-        height: u32,
-    ) -> Result<Engine, Box<dyn Error>> {
-        let ctx = sdl2::init()?;
-        let video = ctx.video()?;
-        let timer = ctx.timer()?;
-        let win = video
-            .window(title, width, height)
-            .position_centered()
-            .build()?;
-        let canvas = win.into_canvas().build()?;
-
-        Ok(Engine {
-            app,
-            title,
-            ctx,
-            video,
-            timer,
-            canvas,
-        })
-    }
-
-    pub fn start(&mut self) -> Result<(), Box<dyn Error>> {
-        // These variables are used to determine the elapsed time between frames, to allow for
-        // time-regulated things like animation
-        let mut last: u64;
-        let mut now = self.timer.performance_counter();
-        let mut elapsed_time: f64;
-        // These variables are used to calculate and display average frame rates
-        let mut time_acc = 0.0;
-        let mut fps_acc = 0.0;
-        let mut fps_counter = 0;
-        // Event handling
-        let mut event_pump = self.ctx.event_pump()?;
-        loop {
-            // Calculate and display FPS
-            last = now;
-            now = self.timer.performance_counter();
-            // Note: I have no idea whether this actually works, so if anyone would like to confirm
-            // or deny this, please do
-            elapsed_time = (now - last) as f64 / self.timer.performance_frequency() as f64;
-            time_acc += elapsed_time;
-            fps_acc += 1.0 / elapsed_time;
-            fps_counter += 1;
-            if time_acc >= 1.0 {
-                let fps = fps_acc / fps_counter as f64;
-                let title = format!("{} ({} FPS)", self.title, fps.round() as u32);
-                // This fails silently on error
-                self.canvas
-                    .window_mut()
-                    .set_title(title.as_str())
-                    .unwrap_or(());
-                time_acc -= 1.0;
-                fps_acc = 0.0;
-                fps_counter = 0;
-            }
-
-            self.app.on_update(&mut self.canvas, elapsed_time)?;
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. } => return Ok(()),
-                    _ => {}
-                }
-            }
-            self.canvas.present();
-        }
-    }
 }
