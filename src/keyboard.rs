199ffb7319c921d6c9d3087a31381bf9072a5f69
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-12-24 16:49:50 +0000

    Add documentation comments.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,11 +1,17 @@
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
+/// Represents the state of a key.
 pub struct Key {
+    /// Was the key pressed on this frame?
     pub pressed: bool,
+    /// Was the key released on this frame?
     pub released: bool,
-    pub held: bool
+    /// Is the key held down?
+    pub held: bool,
 }
 
 impl Key {
+    /// Change the state based on if the key is currently held.
+    /// This is called internally by the engine every frame.
     pub fn update(&mut self, state: bool) {
         self.pressed = state && !self.held;
         self.released = !state && self.held;
