f4a7374b3c1435a11b240328202ac1650c13f3a0
mcb2003 <mikeybuchan@hotmail.co.uk>
2021-01-24 18:59:40 +0000

    Use constant for rectangle size in moving_rect example.

--- examples/moving_rect.rs
+++ examples/moving_rect.rs
@@ -5,6 +5,7 @@ use simple_game_engine::{prelude::*, Application, Engine};
 const MOVEMENT_SPEED: f64 = 200.0;
 const SCREEN_WIDTH: u32 = 480;
 const SCREEN_HEIGHT: u32 = 360;
+const RECT_SIZE: u32 = 100;
 
 struct App {
     x: f64,
@@ -28,18 +29,25 @@ impl Application for App {
         if input.keyboard.held(input::Scancode::Up) {
             self.y = (self.y - MOVEMENT_SPEED * elapsed_time).max(0.0);
         } else if input.keyboard.held(input::Scancode::Down) {
-            self.y = (self.y + MOVEMENT_SPEED * elapsed_time).min(SCREEN_HEIGHT as f64);
+            self.y =
+                (self.y + MOVEMENT_SPEED * elapsed_time).min((SCREEN_HEIGHT - RECT_SIZE) as f64);
         }
         if input.keyboard.held(input::Scancode::Left) {
             self.x = (self.x - MOVEMENT_SPEED * elapsed_time).max(0.0);
         } else if input.keyboard.held(input::Scancode::Right) {
-            self.x = (self.x + MOVEMENT_SPEED * elapsed_time).min(SCREEN_WIDTH as f64);
+            self.x =
+                (self.x + MOVEMENT_SPEED * elapsed_time).min((SCREEN_WIDTH - RECT_SIZE) as f64);
         }
         // Fill the screen
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
         canvas.set_draw_color(Color::RGB(127, 127, 127));
-        canvas.fill_rect(Rect::new(self.x as i32, self.y as i32, 100, 100))?;
+        canvas.fill_rect(Rect::new(
+            self.x as i32,
+            self.y as i32,
+            RECT_SIZE,
+            RECT_SIZE,
+        ))?;
         Ok(())
     }
 }
