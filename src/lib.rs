ac58d85b9fa5de187df0fdb6da6bc519d805b7ec
mcb2003 <mikey@blindcomputing.org>
2021-05-07 18:27:19 +0100

    Add caveat about unifont to crate docs.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,21 @@
 //! # Features
 //! * **Very simple to use:** Just implement the [`Application` trait][Application] on a type of your choice, then pass an instance of this type to [`Engine::new`].
 //! * **Powerful:** Anything you can do with sdl2 from Rust, you can do with this library, and we provide thin abstractions over some of the more convoluted sdl2 interfaces.
-//! * **Built-in text rendering:** No need to find a TTF font and distribute it with your application, just call the [`Canvas::draw_text`][canvas::Canvas::draw_text] method.
+//! * **Built-in text rendering:** No need to find a TTF font and distribute it with your application, just call the [`Canvas::draw_text`][canvas::Canvas::draw_text] method. ([see below](#caveats-with-text-rendering))
+//! ## Caveats With Text Rendering
+//! This crate uses the [GNU Unifont][unifont] for built-in text rendering. As such, if you wish to use this feature, you
+//! must distribute your project under the [GPL][gpl]. As this is not desirable for many projects, this feature is only
+//! enabled if this crate is built with the "unifont" cargo feature.
+//! ```toml
+//! [dependencies.simple-game-engine]
+//! version = "0.8.1"
+//! features = ["unifont"]
+//! ```
+//! If you'd like to render text without using this font, consider checking out the [SDL2 TTF module][sdl2-ttf].
+//!
+//! [unifont]: <http://unifoundry.com/unifont/index.html>
+//! [gpl]: <https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html>
+//! [sdl2-ttf]: <https://docs.rs/sdl2/latest/sdl2/ttf/index.html>
 //! # Example
 //! The simplest SGE program looks like this:
 //! ```no_run
